a8cf33b6c3909f2e984636b3f2e6ec52d8a56c86
jasonaibrahim <jasonaibrahim@gmail.com>
2014-06-26 17:09:56 -0700

    working scraper. still improving selection algorithm. working to limit the number of thumbnails returned as well. version 0.0.1

--- scraper.js
+++ scraper.js
@@ -2,8 +2,9 @@ var http 	= 	require('http'),
 	https 	= 	require('https'),
 	url 	= 	require('url'), 
 	request = 	require('request'), 
-	Q 	=	require('q'),
-	cheerio = 	require('cheerio');
+	Q 		=	require('q'),
+	cheerio = 	require('cheerio'),
+	sizeOf	=	require('image-size');
 
 Array.prototype.uniq = function() {
    var uniq = {}, arr = [];
@@ -24,14 +25,48 @@ function getProtocol(url) {
 
 module.exports.Scraper = function() {
 	var _this = this;
+	this.clean = function(candidates) {
+		var clean = [];
+		for(var i = 0; i < candidates.length; i++) {
+			if(	_this.httpmatch(candidates[i]) && 
+			(	_this.extmatch(candidates[i]) == 'jpg' 	|| 
+				_this.extmatch(candidates[i]) == 'png' 	|| 
+				_this.extmatch(candidates[i]) == 'gif'	)) 
+			{
+				clean.push(_this.httpmatch(candidates[i]));
+			}
+		}
+		return clean.uniq();
+	}
 	this.extmatch = function(path) {
+		if(!path) return null;
 		var extregexp = new RegExp(/.+\.([^?]+)(\?|$)/);
 		return path && path.match(extregexp) && path.match(extregexp)[1];
 	}
 	this.getImages = function(url) {
 		var promise = Q.defer();
-		var candidates = ['thumb1', "thumb2", "thumbN"];
-		promise.resolve(candidates);
+		var candidates = [];
+		if(_this.clean([url]).length > 0){
+			promise.resolve([url]);
+			return promise.promise;
+		}
+		request(url, function(error, response, html) {
+			if(!error && response.statusCode == 200) {
+				var $ = cheerio.load(html);
+				var imagetags = $('img');
+				var metatags = $('meta');
+				for(var i=0; i<imagetags.length; i++){
+					candidates.push(imagetags[i].attribs.src);
+				}
+				for(var i=0; i<metatags.length; i++){
+					candidates.push(metatags[i].attribs.content);
+				}
+				candidates = _this.clean(candidates);
+				promise.resolve(candidates);
+			} else {
+				promise.reject(new Error('Bad Request'));
+			}
+		});
 		return promise.promise;
 	}
 	this.httpmatch = function(url) {
@@ -42,13 +77,45 @@ module.exports.Scraper = function() {
 	this.judge = function(finalists) {
 		var promise = Q.defer();
 		var medalists = [];
+		for(var i=0; i<finalists.length; i++) {
+			medalists.push(finalists[i]);
+		}
 		promise.resolve(medalists);
 		return promise.promise;
+	}
+	this.minSize = function(finalist) {
+		if(finalist.size > 1000) {
+			return true;
+		}
+		return false;
 	}
 	this.narrow = function(candidates) {
 		var promise = Q.defer();
 		var finalists = [];
-		promise.resolve(finalists);
+
+		var seen = 0;
+		for(var i=0; i<candidates.length; i++) {
+			request({
+				url: candidates[i],
+				method: 'HEAD'
+			}, function(error, response, body) {
+				finalists.push({
+					url: response.request.href,
+					size: response.headers['content-length']
+				});
+				if(++seen == candidates.length) {
+					finalists = finalists.filter(_this.minSize);
+					finalists.sort(function(a,b) {
+						return b.size - a.size;
+					});
+					for(var j= 0; j<finalists.length; j++) {
+						finalists[j] = finalists[j].url;
+					}
+					promise.resolve(finalists);
+				}
+			});
+			
+		}
 		return promise.promise;
 	}
 	this.scrape = function(address) {
